a7b0b955d815f99c9dd01662e0d28c7918bdaa1c
Lalani Yash <yashlalani43@gmail.com>
2024-12-24 11:36:57 +0530

    Add project files.

---
+++ Crud_Operation/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Crud_Operation.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 144px">
+    <form id="form1" runat="server">
+        <div>
+            Name<asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Email<asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Address<asp:TextBox ID="txtAddress" runat="server" ></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Button" />
+            <br>
+            
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Crud_Operation/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Crud_Operation
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string s = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\ASP_PROJECT\\Crud_Operation\\Crud_Operation\\App_Data\\Database1.mdf;Integrated Security=True";
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataSet ds;
+        SqlCommand cmd;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getcon();
+
+        }
+        void getcon()
+        {
+            con = new SqlConnection(s);
+            con.Open();
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            getcon();
+            cmd = new SqlCommand("insert into stud_tbl values(Username,Email,Address)values('" + txtName + "','" + txtEmail + "','" + txtAddress + "')", con);
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+}
